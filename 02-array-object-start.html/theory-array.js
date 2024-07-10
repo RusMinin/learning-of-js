c0d4c5b9c0136bd8e0d7bb5d8f939cb5fd1771ca
Ruslan Minin <106439095+RusMinin@users.noreply.github.com>
2024-07-10 14:26:44 +0300

    Commit 2

--- 02-array-object-start.html/theory-array.js
+++ 02-array-object-start.html/theory-array.js
@@ -37,11 +37,84 @@ const names = ['Ruslan', 'Igor', 'Maria', 'Bublik']
 // // console.log(names.includes('Igor')) 
 // // console.log(names.indexOf('Igor!') !== -1)
 
-const people = [
-    {name: 'Ruslan', budget: 4200},
-    {name: 'Maria', budget: 15100},
-    {name: 'Ilia', budget: 300},
-    {name: 'Oleg', budget: 7520},
-]
 
-console.log(people.indexOf({ budget: 7520}))
+
+// let findedPerson
+
+// for (let person of people) {
+//     if (person.budget === 4200){
+//         findedPerson = person
+//     }
+// }
+// console.log(findedPerson)
+// const findedPerson = people.find((p) => p.budget === 4200)
+// console.log(people.indexOf({name: 'Ruslan', budget: 7520}))
+
+// const finded = people.findIndex(function(person) {
+//     return person.budget === 4200
+// })
+// Находим нужный элемент массива по индексу с помощью метода findIndex который возвращает
+// булевое значение и если значение true то элемент находиться а если false то выводит udefind
+// С помощью метода with первым параметром принимаем найденный элемент а вторым параметром 
+// меняем его 
+// const changeArray = people.with(finded, 42)
+// console.log(changeArray)
+
+
+
+// const filtered = people.filter(function(p) {
+//     return p.budget > 5000
+// })
+// Методом filter мы фильтруем массив возвращая элементы массива которые подходят под заданное 
+// условие.Оригинальный массив не меняется а возвращается другой массив
+// console.log(filtered)
+// console.log(people)
+// let sumBudget = 0
+// const filtered = people.filter(function(p) {
+//     return p.budget > 5000
+// })
+// console.log(filtered)
+// filtered.forEach(function(p) {
+//     sumBudget += p.budget
+// })
+// console.log(sumBudget) 
+
+//  const sumBudget = people
+//  .filter(p => p.budget > 5000)
+//  .map(p => p.budget)
+//  .reduce((acc, p) => acc + p, 0)
+
+//  console.log(sumBudget)
+
+//Методом filter возвращаем все элементы массива соответствующие условию, filter возвращает 
+// новый массив не меняя оригинала.Метод map пробегает по всему новому массиву записывая в 
+// в параметр p найденное значение и далее reduce принимает два параметра acc который равен 0
+// и p который равен значению ключа budget .Далее происходит сложение всех найденных значений
+
+
+// const people = [
+//     {name: 'Ruslan', budget: 4200},
+//     {name: 'Maria', budget: 15100}, 
+//     {name: 'Ilia', budget: 300},
+//     {name: 'Oleg', budget: 7520},
+// ]
+
+//  const sumBudget = people.filter(p => p.budget > 5000)
+ 
+//  console.log(sumBudget)
+
+// const byBudget = (p) => p.budget > 5000
+// const pickBudgetNumber = (p) => p.budget
+// const calculation = function(acc, p) { return (acc + p, 0)}
+
+// const sumBudget = people
+// .filter(byBudget)
+// .map(pickBudgetNumber)
+// .reduce(calculation)
+
+// console.log(sumBudget)
+
+// const string = 'Привет как дела'
+// const reversed = string.split('').reverse().join('')
+
+// console.log(reversed)
---
+++ 03-Theory/01-numbers.js
@@ -0,0 +1,65 @@
+// const num = 42
+// const float = 42.42
+// const pow = 10e3
+// const big = 1_000_000
+
+// console.log(Number.MAX_SAFE_INTEGER)
+// console.log(Math.pow(2, 53) - 1)
+// console.log(Number.MIN_SAFE_INTEGER)
+// console.log(Number.MAX_VALUE)
+// console.log(Number.MIN_VALUE)
+// console.log(Number.POSITIVE_INFINITY)
+// console.log(Number.NEGATIVE_INFINITY)
+// console.log(1 / 0)
+// console.log(Number.isFinite(Infinity))
+// const weird = 23 / undefined
+// console.log(Number.isNaN(weird))
+// console.log(Number.isNaN(42))
+
+// const strInt = '42'
+// const strFloat = '42.42'
+
+// console.log(Number(strInt))
+// console.log(Number(strFloat))
+// console.log(Number.parseInt(strFloat))
+// console.log(Number.parseInt(strFloat))
+// console.log(Number.parseFloat(strFloat))
+// console.log(+strInt, +strFloat)
+// console.log(+(0.1 + 0.2).toFixed(1))
+// const fixed =(0.1 + 0.2).toFixed(1)
+// console.log(parseFloat(fixed))
+
+// BigInt
+// console.log(BigInt(Number.MAX_SAFE_INTEGER) + 930293029320n)
+// console.log(-42n)
+// console.log9(42.42n) error
+// console.log(42n - 1) error
+// console.log(parseInt(10n) - 4)
+// console.log(10n - BigInt(4))
+// console.log(5n / 2n)
+// console.log(5 / 2)
+
+// Math
+
+// console.log(Math.E)
+// console.log(Math.PI)
+
+// const myNum = 4.32
+
+// console.log(Math.sqrt(25))
+// console.log(Math.pow(2, 3))
+// console.log(Math.abs(-42))
+// console.log(Math.max(2, 4, 199 ,23))
+// console.log(Math.min(2, 4, 199 ,23))
+// console.log(Math.floor(myNum))
+// console.log(Math.ceil(myNum))
+// console.log(Math.round(myNum))
+// console.log(Math.trunc(myNum))
+// console.log(Math.random())
+
+// function getRandomNumber(min, max) {
+    // return Math.floor(Math.random() * (max - min) + min)
+// }
+
+// const num1 = getRandomNumber(10, 100)
+// console.log(num1)
